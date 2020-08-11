62aa045fb117b2f13b9585b127a9fa74ed9f234b
Rashmi <rashmipatil018@gmail.com>
2020-08-11 16:37:44 -0400

    Updates

---
+++ precache-manifest.6544574b77ac2ed5022474fc6e87174a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1420d57d22c53627ff792c077bd71962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "099613742ed44d4af4ef",
+    "url": "/static/css/main.6b3b8d16.chunk.css"
+  },
+  {
+    "revision": "e6dac5d0b75c6dc29bdb",
+    "url": "/static/js/2.a217fc08.chunk.js"
+  },
+  {
+    "revision": "a5cc24ba963822e8c5ac164bd9e44143",
+    "url": "/static/js/2.a217fc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099613742ed44d4af4ef",
+    "url": "/static/js/main.853929b8.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "76f993317af2636aa3fb5bb9e162cf3c",
+    "url": "/static/media/formslogo.76f99331.jpg"
+  },
+  {
+    "revision": "2b607d6c15ea3ebfb8045aa1c40b98fd",
+    "url": "/static/media/neu.2b607d6c.jpg"
+  },
+  {
+    "revision": "42794950d0daa8bbf7d833bcd1a09a77",
+    "url": "/static/media/news1.42794950.gif"
+  },
+  {
+    "revision": "932561f66caeb3905b2a6f55c36513a5",
+    "url": "/static/media/pratyusha.932561f6.JPG"
+  },
+  {
+    "revision": "403f3b3b1acd773693a0b88f6878ab52",
+    "url": "/static/media/rashmi.403f3b3b.jpg"
+  },
+  {
+    "revision": "2e7392bd69172cb64fb648d6a5abb830",
+    "url": "/static/media/tech.2e7392bd.gif"
+  },
+  {
+    "revision": "ec19b049b1ce4a990f0c9b519da87286",
+    "url": "/static/media/tejas.ec19b049.jpg"
+  },
+  {
+    "revision": "da996d5f28e569cdd32f04273f8ee129",
+    "url": "/static/media/vaibhav.da996d5f.jpg"
+  }
+]);
